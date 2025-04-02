5ddfab3ca35729e3715e2285422b7adf741e5767
learnman18 <manishpan18@gmail.com>
2025-04-02 12:41:31 +0530

    rebase

--- src/components/users/IndividualUser.js
+++ src/components/users/IndividualUser.js
@@ -55,7 +55,7 @@ const IndividualUser = () => {
     return (
         <>
             <div className="pb-5">
-                <h1>updated msg</h1>
+                <h1>rebase</h1>
                 <h2 className="text-32">{individualResponse.name}</h2>
                 <p className="text-lg">Username - {individualResponse.username}</p>
                 <p className="text-lg">Email - {individualResponse.email}</p>
